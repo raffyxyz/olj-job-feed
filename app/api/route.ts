f67a07c5a0f7672228d7647acec86c3b096cddbc
Raffy Amoguis <raffy.amoguis1@gmail.com>
2025-10-06 16:51:41 +0800

    cleanup

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,12 +1,8 @@
 import { saveJobPosts } from "@/lib/api/firestore";
 import { getJobPosts } from "@/lib/api/firestore/get-job-posts";
 import { parseJobPosts, JobType } from "@/lib/parser";
-import {
-  checkJobDate,
-  getOnlyTheNewJobs,
-  delay,
-  checkLastJobFound,
-} from "@/utils/helper";
+import { delay, checkLastJobFound, getDateYesterday } from "@/utils/helper";
+import { checkJobDate } from "@/utils/jobs";
 
 // export const dynamic = "force-static";
 
@@ -19,60 +15,76 @@ export async function GET() {
     const jobs: JobType[] = [];
 
     do {
-      const lastJob = await getJobPosts();
-      console.log("Last job: ", lastJob[0]?.title);
-
-      console.log("Fetching from onlinejobs...");
       const response = await fetch(`${BASE_URL}/${START_PAGE}`);
       const result = await response.text();
 
-      console.log("Parsing job posts...");
       const jobPosts: JobType[] = parseJobPosts(result);
 
-      lastJobIndex = checkLastJobFound(jobPosts, lastJob[0]?.title);
+      const jobDate = checkJobDate(jobPosts);
 
-      if (lastJobIndex < 0) {
-        console.log("Checking for job date...");
-        const jobDate = checkJobDate(jobPosts);
-        if (jobDate === "all-new") {
-          console.log("Jobs are all new...");
-          // Copy all no changes needed.
-          jobs.push(...jobPosts);
+      return Response.json({ date: jobDate });
+    } while (false);
 
-          await saveJobPosts(jobPosts);
-        } else if (jobDate === "had-old") {
-          console.log("Jobs have old date");
-          // Don't copy the old jobs.
-          const newJobs = getOnlyTheNewJobs(jobPosts);
-          jobs.push(...newJobs);
+    // do {
+    //   // Get the last job from firebase.
+    //   const lastJob = await getJobPosts();
+    //   console.log("Last job: ", lastJob[0]?.title);
 
-          await saveJobPosts(newJobs);
-        } else if (jobDate === "all-old") {
-          console.log("Jobs are all old stopping...");
-          // Stop the process.
-          break;
-        }
+    //   // Fetch a request to onlinejobs posting.
+    //   console.log("Fetching from onlinejobs...");
+    //   const response = await fetch(`${BASE_URL}/${START_PAGE}`);
+    //   const result = await response.text();
 
-        console.log("Last job is not found yet..");
+    //   // Parse job posts.
+    //   console.log("Parsing job posts...");
+    //   const jobPosts: JobType[] = parseJobPosts(result);
 
-        console.log("Adding 2 seconds delay...");
-        await delay(2000);
+    //   lastJobIndex = checkLastJobFound(jobPosts, lastJob[0]?.title);
 
-        console.log("Iterating start page...");
-        START_PAGE += 30;
-      } else {
-        console.log("Last job is found at index: ", lastJobIndex);
+    //   console.log("LastJobIndex: ", lastJobIndex);
 
-        console.log("Slicing the job posts from the last index...");
-        const slicedJobPosts = jobPosts.slice(0, lastJobIndex);
-        jobs.push(...slicedJobPosts);
+    //   if (lastJobIndex < 0) {
+    //     console.log("Checking for job date...");
+    //     const jobDate = checkJobDate(jobPosts);
+    //     if (jobDate === "all-new") {
+    //       console.log("Jobs are all new...");
+    //       // Copy all no changes needed.
+    //       jobs.push(...jobPosts);
 
-        console.log("Saving sliced data to database...");
-        await saveJobPosts(slicedJobPosts);
-      }
-    } while (lastJobIndex < 0);
+    //       await saveJobPosts(jobPosts);
+    //     } else if (jobDate === "had-old") {
+    //       console.log("Jobs have old date");
+    //       // Don't copy the old jobs.
+    //       const newJobs = getOnlyTheNewJobs(jobPosts);
+    //       jobs.push(...newJobs);
 
-    return Response.json({ totalJobs: jobs.length, jobs: jobs });
+    //       await saveJobPosts(newJobs);
+    //     } else if (jobDate === "all-old") {
+    //       console.log("Jobs are all old stopping...");
+    //       // Stop the process.
+    //       break;
+    //     }
+
+    //     console.log("Last job is not found yet..");
+
+    //     console.log("Adding 2 seconds delay...");
+    //     await delay(2000);
+
+    //     console.log("Iterating start page...");
+    //     START_PAGE += 30;
+    //   } else {
+    //     console.log("Last job is found at index: ", lastJobIndex);
+
+    //     console.log("Slicing the job posts from the last index...");
+    //     const slicedJobPosts = jobPosts.slice(0, lastJobIndex);
+    //     jobs.push(...slicedJobPosts);
+
+    //     console.log("Saving sliced data to database...");
+    //     await saveJobPosts(slicedJobPosts);
+    //   }
+    // } while (lastJobIndex < 0);
+
+    // return Response.json({ totalJobs: jobs.length, jobs: jobs });
   } catch (err: any) {
     throw new Error(err);
   }
