96a2d3295b842439b35cde1634f29152ff5ae18d
raffyxyz <raffy.amoguis1@gmail.com>
2025-10-14 17:04:49 +0800

    initial design

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
-import { getJobPosts } from "@/lib/api/firestore/get-job-posts";
+
 import { JobBoard } from "@/components/job-board";
 
 export default async function Home() {
-  const jobs = await getJobPosts();
+  
 
-  return <JobBoard data={jobs} />;
+  return <JobBoard data={undefined} />;
 }
--- components/job-board.tsx
+++ components/job-board.tsx
@@ -1,19 +1,54 @@
 "use client";
 
-import { Container, Grid } from "@mantine/core";
+import {
+  ActionIcon,
+  Card,
+  Container,
+  Grid,
+  Group,
+  SimpleGrid,
+  Stack,
+  Text,
+  TextInput,
+  Title,
+} from "@mantine/core";
 import { JobCard } from "./ui/job-card";
 import { JobDocumentData } from "@/lib/types";
+import { IconBriefcase, IconMenu2, IconSettings } from "@tabler/icons-react";
 
-const JobBoard = ({ data }: { data: JobDocumentData[] }) => {
+const JobBoard = ({ data }: { data: JobDocumentData[] | undefined }) => {
   return (
     <Container size="lg" p={"md"}>
-      <Grid>
-        {data.map((job, index) => (
-          <Grid.Col key={index} span={4}>
-            <JobCard job={job} />
-          </Grid.Col>
-        ))}
-      </Grid>
+      <Group justify="space-between">
+        <Title order={1}>Browse Job Posting Today</Title>
+        <ActionIcon variant="transparent" color="dark" aria-label="Menu">
+          <IconSettings />
+        </ActionIcon>
+      </Group>
+      <Group justify="space-between" mt={40} align="end">
+        <TextInput
+          label="Job Title"
+          placeholder="Search for a job title"
+          leftSection={<IconBriefcase size={16} />}
+          aria-label="Search job title"
+        />
+        <Text size="sm" c="dimmed">
+          104 job post available.
+        </Text>
+      </Group>
+
+      <SimpleGrid mt={30} cols={3}>
+        <Card shadow="sm" padding="lg" radius="md" withBorder>
+          <Stack
+            bg="var(--mantine-color-body)"
+            align="stretch"
+            justify="center"
+            gap="md"
+          >
+            <Text fw={500}>Lead Product Manager</Text>
+          </Stack>
+        </Card>
+      </SimpleGrid>
     </Container>
   );
 };
