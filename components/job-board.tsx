9507efe0cbde3fa8571f86e20a8e3c0a5cf38d42
raffyxyz <raffy.amoguis1@gmail.com>
2025-10-15 15:15:53 +0800

    updated layout

--- components/job-board.tsx
+++ components/job-board.tsx
@@ -2,53 +2,136 @@
 
 import {
   ActionIcon,
+  Anchor,
+  Badge,
+  Button,
   Card,
   Container,
+  Flex,
   Grid,
   Group,
-  SimpleGrid,
+  Menu,
   Stack,
   Text,
   TextInput,
   Title,
+  useMantineColorScheme,
 } from "@mantine/core";
-import { JobCard } from "./ui/job-card";
 import { JobDocumentData } from "@/lib/types";
-import { IconBriefcase, IconMenu2, IconSettings } from "@tabler/icons-react";
+import {
+  IconBriefcase,
+  IconBuilding,
+  IconExternalLink,
+  IconMenu2,
+  IconMoonStars,
+  IconSearch,
+  IconSun,
+} from "@tabler/icons-react";
 
 const JobBoard = ({ data }: { data: JobDocumentData[] | undefined }) => {
+  const { colorScheme, toggleColorScheme } = useMantineColorScheme();
+
+  function tagIsEmpty(tags: string[]) {
+    return tags.length === 1 && tags[0] === "";
+  }
   return (
     <Container size="lg" p={"md"}>
       <Group justify="space-between">
         <Title order={1}>Browse Job Posting Today</Title>
-        <ActionIcon variant="transparent" color="dark" aria-label="Menu">
-          <IconSettings />
-        </ActionIcon>
+        <Menu
+          width={200}
+          shadow="md"
+          trigger="hover"
+          openDelay={100}
+          closeDelay={400}
+          position="bottom-end"
+          arrowPosition="center"
+          withArrow
+        >
+          <Menu.Target>
+            <ActionIcon variant="transparent" color="dark" aria-label="Menu">
+              <IconMenu2 />
+            </ActionIcon>
+          </Menu.Target>
+          <Menu.Dropdown>
+            <Menu.Label>Menu</Menu.Label>
+            <Menu.Item
+              leftSection={
+                colorScheme === "light" ? (
+                  <IconMoonStars size={14} />
+                ) : (
+                  <IconSun size={14} />
+                )
+              }
+              onClick={toggleColorScheme}
+            >
+              {colorScheme === "light" ? "Dark Mode" : "Light Mode"}
+            </Menu.Item>
+          </Menu.Dropdown>
+        </Menu>
       </Group>
       <Group justify="space-between" mt={40} align="end">
         <TextInput
           label="Job Title"
           placeholder="Search for a job title"
-          leftSection={<IconBriefcase size={16} />}
+          leftSection={<IconSearch size={16} />}
           aria-label="Search job title"
         />
         <Text size="sm" c="dimmed">
-          104 job post available.
+          {data?.length} job post available.
         </Text>
       </Group>
 
-      <SimpleGrid mt={30} cols={3}>
-        <Card shadow="sm" padding="lg" radius="md" withBorder>
-          <Stack
-            bg="var(--mantine-color-body)"
-            align="stretch"
-            justify="center"
-            gap="md"
-          >
-            <Text fw={500}>Lead Product Manager</Text>
-          </Stack>
-        </Card>
-      </SimpleGrid>
+      <Grid mt={30}>
+        {data?.map((job, index) => (
+          <Grid.Col span={4} key={index}>
+            <Card shadow="sm" padding="lg" radius="md" h="auto" withBorder>
+              <Stack align="stretch" justify="center" gap="md">
+                <div>
+                  <Text fw={600} size="lg">
+                    {job.title}
+                  </Text>
+                  <Flex gap={4} align="center">
+                    <IconBuilding size={16} />
+                    <Text size="sm">{job.posterName}</Text>
+                  </Flex>
+                  <Flex gap={4} align="center">
+                    <IconBriefcase size={16} />
+                    <Text size="sm">{job.employmentType}</Text>
+                  </Flex>
+                </div>
+                <Text>{job.salary}</Text>
+                {!tagIsEmpty(job.tags) && (
+                  <Flex
+                    gap={4}
+                    justify="flex-start"
+                    align="center"
+                    direction="row"
+                    wrap="wrap"
+                  >
+                    {job.tags.map((tag, idx) => (
+                      <Badge key={idx} size="sm" variant="light">
+                        {tag}
+                      </Badge>
+                    ))}
+                  </Flex>
+                )}
+                <Anchor
+                  href={`https://onlinejobs.ph${job.url}`}
+                  target="_blank"
+                >
+                  <Button
+                    rightSection={<IconExternalLink size={14} />}
+                    radius="xl"
+                  >
+                    More Details
+                  </Button>
+                </Anchor>
+              </Stack>
+            </Card>
+          </Grid.Col>
+        ))}
+      </Grid>
     </Container>
   );
 };
