a97419c3ec0b19a5646e29eea5e1b0cf88e37658
raffyxyz <raffy.amoguis1@gmail.com>
2025-10-15 15:49:06 +0800

    added api calls

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,18 @@
-
 import { JobBoard } from "@/components/job-board";
+import { JobDocumentData } from "@/lib/types";
+
+const fetchJobPosting = async (): Promise<JobDocumentData[]> => {
+  const response = await fetch(`${process.env.API_ENDPOINT}/api`, {
+    // cache: "force-cache",
+  });
+
+  const data = await response.json();
+
+  return data.jobs;
+};
 
 export default async function Home() {
-  
+  const jobs = await fetchJobPosting();
 
-  return <JobBoard data={undefined} />;
+  return <JobBoard data={jobs} />;
 }
