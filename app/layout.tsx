467770a5565dbe0e00837f775e61b02892c08ad8
raffyxyz <raffy.amoguis1@gmail.com>
2025-10-15 15:49:30 +0800

    updated font

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,9 @@ import "@mantine/core/styles.css";
 
 import {
   ColorSchemeScript,
+  DEFAULT_THEME,
   MantineProvider,
+  createTheme,
   mantineHtmlProps,
 } from "@mantine/core";
 
@@ -12,6 +14,15 @@ export const metadata = {
   title: "OLJ App",
   description: "I have followed setup instructions carefully",
 };
+
+const theme = createTheme({
+  fontFamily: "Arimo, sans-serif",
+  fontFamilyMonospace: "Monaco, Courier, monospace",
+  headings: {
+    // Use default theme if you want to provide default Mantine fonts as a fallback
+    fontFamily: `Arimo, ${DEFAULT_THEME.fontFamily}`,
+  },
+});
 
 export default function RootLayout({
   children,
@@ -21,10 +32,20 @@ export default function RootLayout({
   return (
     <html lang="en" {...mantineHtmlProps}>
       <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin=""
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Arimo:ital,wght@0,400..700;1,400..700&display=swap"
+          rel="stylesheet"
+        />
         <ColorSchemeScript />
       </head>
       <body>
-        <MantineProvider>{children}</MantineProvider>
+        <MantineProvider theme={theme}>{children}</MantineProvider>
       </body>
     </html>
   );
