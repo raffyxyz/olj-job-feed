7bf4a6fd705abecd43eb7f0d4c93a1ecf89b7f22
raffyxyz <raffy.amoguis1@gmail.com>
2025-10-18 15:50:30 +0800

    removed fetch from server

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,7 @@
-import { JobDocumentData } from "@/shared/types";
 import { JobBoard } from "@/screens/job-board";
 
-const fetchJobPosting = async (): Promise<JobDocumentData[]> => {
-  const response = await fetch(`${process.env.API_ENDPOINT}/api`, {});
-
-  const data = await response.json();
-
-  return data.jobs;
-};
-
 const Page = async () => {
-  const jobs = await fetchJobPosting();
-
-  return <JobBoard data={jobs} />;
+  return <JobBoard />;
 };
 
 export default Page;
