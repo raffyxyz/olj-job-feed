ce0385b3e9dc918014802d8671cfcc66701ccb1a
raffyxyz <raffy.amoguis1@gmail.com>
2025-10-20 09:52:19 +0800

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
+import { Suspense } from "react";
 import { JobBoard } from "@/screens/job-board";
 
 const Page = async () => {
-  return <JobBoard />;
+  return (
+    <Suspense>
+      <JobBoard />
+    </Suspense>
+  );
 };
 
 export default Page;
