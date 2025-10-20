a60df186ef80c4fc59169fc2d648871aee7092f2
raffyxyz <raffy.amoguis1@gmail.com>
2025-10-20 11:07:45 +0800

    changed queryFn to server actions

--- entities/job/queries/use-jobs-query.ts
+++ entities/job/queries/use-jobs-query.ts
@@ -1,11 +1,11 @@
 import { useQuery } from "@tanstack/react-query";
-import { jobApi } from "../api/job-api";
 import { queryKeys } from "../query-keys/query-keys";
+import { fetchJobs } from "../api/fetch-jobs";
 
 export const useJobsQuery = () => {
   return useQuery({
     queryKey: queryKeys.jobs.all(),
-    queryFn: () => jobApi.getJobs(),
+    queryFn: () => fetchJobs(),
     staleTime: 5 * 60 * 1000,
   });
 };
