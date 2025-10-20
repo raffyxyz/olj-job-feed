da005bda4fd0fb4f6ac10b66c70859b7894fa8da
raffyxyz <raffy.amoguis1@gmail.com>
2025-10-20 14:51:03 +0800

    updated error catching

--- entities/job/api/fetch-jobs.ts
+++ entities/job/api/fetch-jobs.ts
@@ -2,16 +2,21 @@
 
 import { JobType } from "@/shared/types";
 
-export async function fetchJobs(): Promise<JobType[]> {
+export async function fetchJobs(): Promise<JobType[] | undefined> {
   console.log("API_ENDPOINT: ", process.env.API_ENDPOINT);
-  const response = await fetch(`${process.env.API_ENDPOINT}/api`, {
-    method: "GET",
-    headers: {
-      Authorization: `Bearer ${process.env.API_KEY}`,
-    },
-  });
+  try {
+    const response = await fetch(`${process.env.API_ENDPOINT}/api`, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${process.env.API_KEY}`,
+      },
+    });
 
-  const data = await response.json();
+    const data = await response.json();
 
-  return data.jobs;
+    return data.jobs;
+  } catch (err) {
+    console.log("Error occured");
+    console.log(err);
+  }
 }
