11b1083be4f72c6a16daa200243516f487643fa5
raffyxyz <raffy.amoguis1@gmail.com>
2025-10-21 09:50:17 +0800

    added console.log for env

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,6 +1,8 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest) {
+  console.log("ENDPOINT: ", process.env.API_ENDPOINT);
+  console.log("BEARER TOKEN: ", process.env.BEARER_TOKEN);
   const response = await fetch(`${process.env.API_ENDPOINT}/api/jobs`, {
     method: "GET",
     headers: {
