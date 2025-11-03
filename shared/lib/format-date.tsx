0599c7a01a81af8721720bf19c045e9c296a27c2
raffyxyz <raffy.amoguis1@gmail.com>
2025-11-03 10:23:39 +0800

    cleanup

--- shared/lib/format-date.tsx
+++
@@ -1,9 +0,0 @@
-// Format date similar to what onlinejobs.ph is using.
-export function formatDate(date: Date) {
-  const formatted = date.toLocaleDateString("en-US", {
-    month: "short",
-    day: "2-digit",
-    year: "numeric",
-  });
-  return formatted;
-}
